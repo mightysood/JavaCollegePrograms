8364e1e7a2ffab8cbcd12f59cf64c6a7ef6ce584
vaibhavsood1382 <32262801+vaibhavsood1382@users.noreply.github.com>
2018-08-07 16:06:04 +0530

    Add files via upload
    Max diff b/w 2 array elements in o(n) time and o(1) complexity

---
+++ maxdiff.java
@@ -0,0 +1,32 @@
+import java.io.*;
+/* constraints--
+a[i]-a[j] such that i<j
+using O(n) time and o(1) time complexity
+*/
+class maxdiff{
+	public static void main(String[] args) throws IOException
+	{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		
+		int arr[] = new int[n];
+		
+		for(int i=0; i<n; i++)
+			arr[i] = Integer.parseInt(br.readLine());
+		
+		int maxdiff = -1, max = arr[n-1];
+		
+		for(int i = n-2; i>=0; i--)
+		{
+			if(arr[i] > max)
+				max = arr[i];
+			else
+			{
+				int diff = max - arr[i];
+				if(diff> maxdiff)
+					maxdiff = diff;	
+			}
+		}
+		System.out.println(maxdiff);
+	}
+}
