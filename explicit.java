19837935e9799d0f73a26321b697cdd553bae060
vaibhavsood1382 <32262801+vaibhavsood1382@users.noreply.github.com>
2018-08-07 13:29:06 +0530

    Add files via upload

---
+++ explicit.java
@@ -0,0 +1,10 @@
+import java.io.*;
+
+class explicit{
+public static void main(String Args[]){
+    int x = 15;
+    double m = (double) x;
+    System.out.println("The int is "+x+" Now explicitly will typecast into double "+m);
+
+}
+}
---
+++ implicit.java
@@ -0,0 +1,9 @@
+import java.io.*;
+
+class implicit{
+    public static void main(String Args[]){
+        int x = 5;
+        double d = x;
+        System.out.println("The int it "+x+" Now implicitly converted to double "+d);
+    }
+}
